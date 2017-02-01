20148f58ca7f25f96dfdb1867864277c3244e86a
rane <pickleboy77@gmail.com>
2017-01-31 22:15:16 -0900

    Replace the removed getData() method with getWoolData() This resolves issue #10

--- src/main/java/grokswell/hypermerchant/ShopMenu.java
+++ src/main/java/grokswell/hypermerchant/ShopMenu.java
@@ -247,7 +247,7 @@ public class ShopMenu implements Listener, MerchantMenu {
 		
 		ItemStack stack;
 	    while (count < size-9) {
-			stack = new ItemStack(Material.STAINED_GLASS_PANE, 1, DyeColor.SILVER.getData());
+			stack = new ItemStack(Material.STAINED_GLASS_PANE, 1, DyeColor.SILVER.getWoolData());
 	    	this.setOption(count, stack, " ", " ");
 	    	count++;
 	    }
